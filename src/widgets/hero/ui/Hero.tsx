356e21c24014a4bc9cd5f433af3b3a74304c55ef
Nowruz Owliyagulyyew <161121707+Novruz3@users.noreply.github.com>
2026-04-11 15:51:50 +0500

    third

--- src/widgets/hero/ui/Hero.tsx
+++ src/widgets/hero/ui/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
       <div className="max-w-6xl mx-auto bg-linear-to-r from-purple-600 to-indigo-500 rounded-3xl p-8 flex flex-col md:flex-row items-center justify-between">
         <div className="text-white max-w-md">
           <h1 className="text-3xl font-bold mb-4">
-            Programming school for kids from 7 to 14 years old
+            Programming school for kids from 9 to 15 years old
           </h1>
           <p className="mb-6">We teach children programming through practice</p>
           <Button
